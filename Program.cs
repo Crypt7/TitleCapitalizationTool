104d0e68810da19e193330a62f0959d2937564bf
Vitalii.Rogozhyn <vitalii.rogozhyn@gmail.com>
2018-04-13 00:30:12 +0300

    Close #21

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace TitleCapitalizationTool
                 int fixedCursorLeft = Console.CursorLeft;
                 int fixedCursorTop = Console.CursorTop;
                 gotTextLine = Console.ReadLine();
-                if (string.IsNullOrEmpty(gotTextLine) || string.IsNullOrWhiteSpace(gotTextLine))
+                if (string.IsNullOrEmpty(gotTextLine))
                 {
                     Console.CursorLeft = fixedCursorLeft;
                     Console.CursorTop = fixedCursorTop;
@@ -39,11 +39,14 @@ namespace TitleCapitalizationTool
             Console.Write("Capitalized title: ");
             Console.ForegroundColor = ConsoleColor.Green;
             string gotTextLineCopy = gotTextLine;
+            if (!string.IsNullOrWhiteSpace(gotTextLineCopy))
+            {
             gotTextLineCopy = gotTextLineCopy.Trim();
             gotTextLineCopy = gotTextLineCopy.ToLower();
             gotTextLineCopy = SpaceNormalization(gotTextLineCopy);
             gotTextLineCopy = SeparatorNormalization(gotTextLineCopy);
             gotTextLineCopy = CaseNormalization(gotTextLineCopy);
+            }
             Console.WriteLine(gotTextLineCopy);
             Console.WriteLine();
             StringCapitalization();
@@ -57,13 +60,18 @@ namespace TitleCapitalizationTool
             for (int i = 0; i <= arrayLendth; i++)
             {
                 string currentWord = wordsArray[i];
+
+
                 if (CheckIfLowercase(currentWord) && i != arrayLendth)
                 {
                     constructedString.Append(currentWord + ' ');
                 }
                 else
                 {
-                    constructedString.Append(UpperFirstWordLetter(currentWord) + ' ');
+                    if (currentWord.Length != 0)
+                    {
+                        constructedString.Append(UpperFirstWordLetter(currentWord) + ' ');
+                    }
                 }
             }
             constructedString[0] = char.ToUpper(constructedString[0]);
@@ -72,6 +80,7 @@ namespace TitleCapitalizationTool
 
         private static string UpperFirstWordLetter(string processedString)
         {
+
             StringBuilder constructedString = new StringBuilder(processedString);
             if (char.IsLetter(constructedString[0]))
             {
@@ -96,6 +105,7 @@ namespace TitleCapitalizationTool
 
         private static string SeparatorNormalization(string processedString)
         {
+
             string[] wordsArray = processedString.Split(' ');
             StringBuilder constructedString = new StringBuilder();
             foreach (string currentWord in wordsArray)
@@ -103,39 +113,40 @@ namespace TitleCapitalizationTool
                 int currentWordLendth = currentWord.Length;
                 for (int i = 0; i < currentWordLendth; i++)
                 {
-                    if (CheckIfSeparator(currentWord[i]))
-                    {
-                        if (currentWord[i] == '-')
-                        {
-                            constructedString.Append(" - ");
-                        }
-                        else
-                        {
-                            if ((i + 1) != currentWordLendth && char.IsLetter(currentWord[i + 1]))
-                            {
-                                constructedString.Append(currentWord[i]);
-                                constructedString.Append(' ');
-                                Console.WriteLine(currentWord[i]);
-                            }
-                            else
-                            {
-                                constructedString.Append(currentWord[i]);
-                            }
-                        }
-                    }
-                    else
+                    if (!CheckIfSeparator(currentWord[i]))
                     {
                         constructedString.Append(currentWord[i]);
                     }
+                    else
+                    {
+                        constructedString=AddSeparator(constructedString, currentWord[i]);
+                    }
+   
                 }
                 constructedString.Append(' ');
             }
             return constructedString.ToString().Trim();
         }
 
+        private static StringBuilder AddSeparator(StringBuilder constructedString, char currentSeparator)
+        {
+            int constructedStringLendth = constructedString.Length;
+            StringBuilder ModifiedString = new StringBuilder(constructedString.ToString().TrimEnd());
+            if (currentSeparator != '-')
+            {
+                ModifiedString.Append(currentSeparator);
+                ModifiedString.Append(' ');
+            }
+            else
+            {
+                ModifiedString.Append(" - ");
+            }
+            return ModifiedString;
+        }
+
         private static bool CheckIfLowercase(string processedString)
         {
-            string[] lowerCaseElementsList = { "a", "an",  "and","at", "but", "by", "for", "in", "nor",  "of", "on", "or", "out", "so", "the", "to", "up", "yet" };
+            string[] lowerCaseElementsList = {"a","an","and","at","but","by","for","in","nor","of","on","or","out","so","the","to","up","yet"};
             bool isLowerCase = false;
             foreach (string currentlowerCaseElement in lowerCaseElementsList)
             {
@@ -150,7 +161,7 @@ namespace TitleCapitalizationTool
 
         private static bool CheckIfSeparator(char symbolToCheck)
         {
-            char[] SeparatorsList = { '.', ',', ';', ':', '!', '?', '-' };
+            char[] SeparatorsList = {'.',',',';',':','!','?','-'};
             bool isSeparator = false;
             foreach (char currentSeparator in SeparatorsList)
             {
